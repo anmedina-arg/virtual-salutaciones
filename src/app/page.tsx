a8b18acf2cece2586332b189afb483874680cb47
anmedina-arg <andres.medina.arg@gmail.com>
2023-12-14 10:33:08 -0300

    add instructions text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,13 +24,16 @@ export default function Home() {
 
   return (
     <main className="flex flex-col min-h-screen">
-      <h1 className="text-gray-300 text-center items-center justify-center w-screen absolute">bienvenido al saludador de Virtual!</h1>
-      <form onSubmit={handleSubmit} className="flex gap-2 items-center justify-center min-h-screen">
-        <input type="text" onChange={handleChange} value={filtro} className="p-2 rounded-md"/>
-        <Link href={`/${filtro}`}>
-          <Button label="ver mi saludo"/>
-        </Link>
-      </form>
+      <h1 className="text-gray-200 text-center items-center justify-center w-screen absolute text-2xl top-4">Bienvenido al saludador de Virtual!</h1>
+      <div className="h-screen flex flex-col justify-center items-center gap-10">
+        <p className="text-gray-200 w-1/2 text-2xl text-center ">Por favor ingresa tus ultimos 3 del DNI para que veas el mensajito que preparamos para vos!</p>
+        <form onSubmit={handleSubmit} className="flex gap-2 items-center justify-center ">
+          <input type="text" onChange={handleChange} value={filtro} className="p-2 rounded-md" />
+          <Link href={`/${filtro}`}>
+            <Button label="ver mi saludo"/>
+          </Link>
+        </form>
+      </div>
     </main>
   );
 }
